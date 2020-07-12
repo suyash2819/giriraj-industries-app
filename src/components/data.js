2097b2bf79a1a55f4e3e86bfb3189017865f28d8
suyash mandhana <mandhanasuyash@yahoo.in>
2020-07-12 17:37:33 +0530

    fixed data.js

---
+++ src/components/data.js
@@ -0,0 +1,83 @@
+export const data = {
+  collections: {
+    men: [
+      {
+        key: 1,
+        itemType: "Lower",
+        Description: "cotton , color red, washable...",
+        Cost: "250 rs",
+        btnText: "Add To Cart",
+        image: "https://picsum.photos/200/300?q=$(Math.random())",
+      },
+      {
+        key: 2,
+        itemType: "Tshirt",
+        Description: "Lenin , color white, washable...",
+        Cost: "899 rs",
+        btnText: "Add To Cart",
+        image: "https://picsum.photos/200/300?q=$(Math.random())",
+      },
+    ],
+    women: [
+      {
+        key: 3,
+        itemType: "Lower",
+        Description: "cotton , color red, washable...",
+        Cost: "250 rs",
+        btnText: "Add To Cart",
+        image: "https://picsum.photos/200/300?q=$(Math.random())",
+      },
+      {
+        key: 4,
+        itemType: "Top",
+        Description: "Lenin , color white, washable...",
+        Cost: "899 rs",
+        btnText: "Add To Cart",
+        image: "https://picsum.photos/200/300?q=$(Math.random())",
+      },
+    ],
+    kids: [
+      {
+        key: 5,
+        itemType: "Lower",
+        Description: "cotton , color red, washable...",
+        Cost: "250 rs",
+        btnText: "Add To Cart",
+        image: "https://picsum.photos/200/300?q=$(Math.random())",
+      },
+      {
+        key: 6,
+        itemType: "Tshirt",
+        Description: "Lenin , color white, washable...",
+        Cost: "899 rs",
+        btnText: "Add To Cart",
+        image: "https://picsum.photos/200/300?q=$(Math.random())",
+      },
+    ],
+    covid: [
+      {
+        key: 7,
+        itemType: "Lower",
+        Description: "cotton , color red, washable...",
+        Cost: "250 rs",
+        btnText: "Add To Cart",
+        image: "https://picsum.photos/200/300?q=$(Math.random())",
+      },
+      {
+        key: 8,
+        itemType: "Tshirt",
+        Description: "Lenin , color white, washable...",
+        Cost: "899 rs",
+        btnText: "Add To Cart",
+        image: "https://picsum.photos/200/300?q=$(Math.random())",
+      },
+    ],
+  },
+};
+
+export const sections = {
+  men: "Men",
+  women: "Women",
+  kids: "Kids",
+  covid: "Covid",
+};
